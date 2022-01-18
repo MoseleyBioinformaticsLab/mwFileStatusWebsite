dada97bd8af2ba589135d653e7059124625d89a4
Christian D. Powell <christian.david.powell@gmail.com>
2022-01-18 14:25:51 -0500

    Adds methods for comparing two mwTab files.

---
+++ mwFileStatusWebsite/compare.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+compare.py
+~~~~~~~~~~
+
+This script contains methods comparing two Metabolomics Workbench file in `~mwtab.mwtab.MWTabFile` object and assert
+that the files (objects) are the same.
+"""
+import mwtab
+import operator
+
+
+ITEM_SECTIONS = {
+    # "METABOLOMICS WORKBENCH",
+    "PROJECT",
+    "STUDY",
+    "ANALYSIS",
+    "SUBJECT",
+    "COLLECTION",
+    "TREATMENT",
+    "SAMPLEPREP",
+    "CHROMATOGRAPHY",
+    "MS",
+    "NM"
+}
+
+DATA_BLOCKS = {
+    'MS_METABOLITE_DATA',
+    'NMR_BINNED_DATA',
+    'NMR_METABOLITE_DATA'
+}
+
+
+def compare_blocks(mwtabfile_1, mwtabfile_2):
+    """Method for comparing the section headers of two Metabolomics Workbench file in `~mwtab.mwtab.MWTabFile` object
+    format. Raises AssertionError if false.
+
+    :param mwtabfile_1: First Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_1: :py:class:`~mwtab.mwtab.MWTabFile`
+    :param mwtabfile_2: Second Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_2: :py:class:`~mwtab.mwtab.MWTabFile`
+    :return: None
+    """
+    if not mwtabfile_1.keys() == mwtabfile_2.keys():
+        raise AssertionError(
+            "mwTab files contain different blocks: \"{}\"".format(
+                mwtabfile_1.keys() ^ mwtabfile_2.keys()  # symmetric difference
+            )
+        )
+
+def compare_block_items(mwtabfile_1, mwtabfile_2):
+    """Method for comparing the items (keys and values) present within the sections of two Metabolomics Workbench file
+    in `~mwtab.mwtab.MWTabFile` object format.
+
+    :param mwtabfile_1: First Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_1: :py:class:`~mwtab.mwtab.MWTabFile`
+    :param mwtabfile_2: Second Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_2: :py:class:`~mwtab.mwtab.MWTabFile`
+    :return: List of errors.
+    :rtype: list
+    """
+    error_list = list()
+
+    for section_key in set(mwtabfile_1.keys()) & set(mwtabfile_2.keys()) & ITEM_SECTIONS:
+        if mwtabfile_1[section_key].items() ^ mwtabfile_2[section_key].items():
+            error_list.append(
+                AssertionError(
+                    "Sections \"{}\" contain missmatched items: {}".format(
+                        section_key,
+                        mwtabfile_1[section_key].items() ^ mwtabfile_2[section_key].items()
+                    )
+                )
+            )
+
+    return error_list
+
+
+def compare_subject_sample_factors(mwtabfile_1, mwtabfile_2):
+    """Method for comparing the items (keys and values) present within the sections of two Metabolomics Workbench file
+    in `~mwtab.mwtab.MWTabFile` object format.
+
+    :param mwtabfile_1: First Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_1: :py:class:`~mwtab.mwtab.MWTabFile`
+    :param mwtabfile_2: Second Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_2: :py:class:`~mwtab.mwtab.MWTabFile`
+    :return: None
+    """
+    new_mwtabfile_1_SSF_list = sorted(mwtabfile_1['SUBJECT_SAMPLE_FACTORS'], key=operator.itemgetter('Sample ID'))
+    new_mwtabfile_2_SSF_list = sorted(mwtabfile_2['SUBJECT_SAMPLE_FACTORS'], key=operator.itemgetter('Sample ID'))
+
+    if new_mwtabfile_1_SSF_list != new_mwtabfile_2_SSF_list:
+        raise AssertionError("mwTab files contain different 'SUBJECT_SAMPLE_FACTORS' sections.")
+
+
+def compare_data(mwtabfile_1, mwtabfile_2):
+    """Method for comparing "_DATA" blocks present within two Metabolomics Workbench file in `~mwtab.mwtab.MWTabFile`
+    object format.
+
+    :param mwtabfile_1: First Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_1: :py:class:`~mwtab.mwtab.MWTabFile`
+    :param mwtabfile_2: Second Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_2: :py:class:`~mwtab.mwtab.MWTabFile`
+    :return: List of errors.
+    :rtype: list
+    """
+    data_section = list(set(mwtabfile_1.keys()) & set(mwtabfile_2.keys()) & DATA_BLOCKS)[0]
+
+    # data block is not consistently present across files
+    if type(data_section) != str or not data_section:
+        return [AssertionError("Unable to find '_DATA' block in given files.")]
+
+    else:
+        error_list = list()
+        # compare "Units"
+        if mwtabfile_1[data_section]['Units'] != mwtabfile_2[data_section]['Units']:
+            error_list.append(AssertionError("'Units' section of '{}' block do not match.".format(data_section)))
+
+        # compare "Metabolites"
+        new_mwtabfile_1_metabolites_list = sorted(mwtabfile_1[data_section]['Metabolites'],
+                                                  key=operator.itemgetter('Metabolite'))
+        new_mwtabfile_2_metabolites_list = sorted(mwtabfile_2[data_section]['Metabolites'],
+                                                  key=operator.itemgetter('Metabolite'))
+        if new_mwtabfile_1_metabolites_list != new_mwtabfile_2_metabolites_list:
+            error_list.append(AssertionError("'Metabolites' section of '{}' block do not match.".format(data_section)))
+
+        # compare "Data"
+        new_mwtabfile_1_data_list = sorted(mwtabfile_1[data_section]['Data'],
+                                                  key=operator.itemgetter('Metabolite'))
+        new_mwtabfile_2_data_list = sorted(mwtabfile_2[data_section]['Data'],
+                                                  key=operator.itemgetter('Metabolite'))
+        if new_mwtabfile_1_data_list != new_mwtabfile_2_data_list:
+            error_list.append(AssertionError("'Data' section of '{}' block do not match.".format(data_section)))
+
+        return error_list
+
+
+def compare(mwtabfile_1, mwtabfile_2):
+    """Method for comparing two Metabolomics Workbench file in `~mwtab.mwtab.MWTabFile` object format to assert that
+    they are the same. Raises AssertionError if false.
+
+    :param mwtabfile_1: First Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_1: :py:class:`~mwtab.mwtab.MWTabFile`
+    :param mwtabfile_2: Second Metabolomics Workbench file in mwtab format to be compared.
+    :type mwtabfile_2: :py:class:`~mwtab.mwtab.MWTabFile`
+    :return: Log of assertion errors.
+    """
+    error_list = list()
+
+    # compare files to assert they have the same section headings.
+    try:
+        compare_blocks(mwtabfile_1, mwtabfile_2)
+    except Exception as e:
+        error_list.append(e)
+
+    # compare files to assert they have the same items in each  appropriate block (e.g. PROJECT, STUDY, etc.).
+    error_list.extend(compare_block_keys(mwtabfile_1, mwtabfile_2))  # no issue if compare_block_keys() returns a null list.
+
+    # check that the SUBJECT_SAMPLE_FACTORS sections match
+    try:
+        compare_subject_sample_factors(mwtabfile_1, mwtabfile_2)
+    except Exception as e:
+        error_list.extend(e)
+
+    error_list.extend(compare_data(mwtabfile_1, mwtabfile_2))
---
+++ tests/test_compare.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_compare.py
+~~~~~~~~~~~~~~~
+
+This script contains methods comparing two Metabolomics Workbench file in `~mwtab.mwtab.MWTabFile` object and assert
+that the files (objects) are the same.
+"""
+import pytest
+import mwFileStatusWebsite
+import mwtab
+from urllib.request import urlopen
+from time import sleep
+import pkgutil
+from collections import OrderedDict
+
+
+@pytest.mark.parametrize('mwtabfile_1, mwtabfile_2, error', [
+    ({"a": {"b": 1}}, {"a": {"b": 1}}, False),  # same blocks, same items
+    ({"a": {"b": 1}}, {"a": {"c": 1}}, False),  # same blocks, different item blocks
+    ({"a": {"b": 1}}, {"b": {"b": 1}}, True),  # different blocks
+    ({"a": {"b": 1}}, {"a": {"b": 1}, "c": {"b": 1}}, True), # one file has an additional block
+    ({"a": {"b": 1}}, {}, True),  # one file is null
+])
+def test_compare_blocks(mwtabfile_1, mwtabfile_2, error):
+    try:
+        mwFileStatusWebsite.compare.compare_blocks(mwtabfile_1, mwtabfile_2)
+        if error:  # test should have raise an error
+            assert False
+    except Exception as e:
+        if not error:
+            assert False
+
+
+@pytest.mark.parametrize('mwtabfile_1, mwtabfile_2, error', [
+    ({"PROJECT": {"b": 1}}, {"PROJECT": {"b": 1}}, False),  # same blocks, same items
+    ({"PROJECT": {"b": 1}}, {"PROJECT": {"c": 1}}, True),  # same blocks, different item blocks
+    ({"PROJECT": {"b": 1}}, {"PROJECT": {}}, True),  # same blocks, one block is null
+    ({"PROJECT": {"b": 1}}, {"PROJECT": {"b": 1}, "STUDY": {"a": 1}}, False),  # different blocks, same items in mutual blocks
+])
+def test_compare_block_items(mwtabfile_1, mwtabfile_2, error):
+    errors = mwFileStatusWebsite.compare.compare_block_items(mwtabfile_1, mwtabfile_2)
+    if bool(errors) != error:
+        assert False
+
+
+@pytest.mark.parametrize('mwtabfile_1, mwtabfile_2, error', [
+    # same SSF section
+    (
+        {'SUBJECT_SAMPLE_FACTORS':[
+            OrderedDict([('Subject ID', '-'), ('Sample ID', '1'), ('Factors', {'Factor1': 'Blah'})])
+        ]},
+        {'SUBJECT_SAMPLE_FACTORS': [
+            OrderedDict([('Subject ID', '-'), ('Sample ID', '1'), ('Factors', {'Factor1': 'Blah'})])
+        ]},
+        False
+    ),
+    # different 'Sample ID'
+    (
+        {'SUBJECT_SAMPLE_FACTORS': [
+            OrderedDict([('Subject ID', '-'), ('Sample ID', '1'), ('Factors', {'Factor1': 'Blah'})])
+        ]},
+        {'SUBJECT_SAMPLE_FACTORS': [
+            OrderedDict([('Subject ID', '-'), ('Sample ID', '2'), ('Factors', {'Factor1': 'Blah'})])
+        ]},
+        True
+    ),
+    # different factor values
+    (
+        {'SUBJECT_SAMPLE_FACTORS': [
+            OrderedDict([('Subject ID', '-'), ('Sample ID', '1'), ('Factors', {'Factor1': 'Blah'})])
+        ]},
+        {'SUBJECT_SAMPLE_FACTORS': [
+            OrderedDict([('Subject ID', '-'), ('Sample ID', '1'), ('Factors', {'Factor1': 'Meh'})])
+        ]},
+        True
+    )
+])
+def test_compare_block_items(mwtabfile_1, mwtabfile_2, error):
+    try:
+        mwFileStatusWebsite.compare.compare_subject_sample_factors(mwtabfile_1, mwtabfile_2)
+        if error:  # test should have raise an error
+            assert False
+    except Exception as e:
+        if not error:
+            assert False
+
+
+@pytest.mark.parametrize('mwtabfile_1, mwtabfile_2, error', [
+    # same data section
+    (
+        {'MS_METABOLITE_DATA': {
+            'Units': 'test',
+            'Data': [
+                OrderedDict([
+                ('Metabolite', 'Test'),
+                ('Subject_1', 1000)
+                ]),
+            ],
+            'Metabolites': [
+                OrderedDict([
+                ('Metabolite', 'Test'),
+                ('Value_1', 'Test')
+                ]),
+            ],
+        }},
+        {'MS_METABOLITE_DATA': {
+            'Units': 'test',
+            'Data': [
+                OrderedDict([
+                ('Metabolite', 'Test'),
+                ('Subject_1', 1000)
+                ]),
+            ],
+            'Metabolites': [
+                OrderedDict([
+                ('Metabolite', 'Test'),
+                ('Value_1', 'Test')
+                ]),
+            ],
+        }},
+        False
+    )
+])
+def test_compare_block_items(mwtabfile_1, mwtabfile_2, error):
+    errors = mwFileStatusWebsite.compare.compare_data(mwtabfile_1, mwtabfile_2)
+
+    if errors and not error:
+        assert False
+
+    if not errors and error:
+        assert False
